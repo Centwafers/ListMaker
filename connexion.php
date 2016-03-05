e5c6d1e8db28131df88530c89dca196651d12b00
Centwafers <lenaertsm@hotmail.be>
2016-03-05 14:02:06 +0100

    Correction d'un oubli
    ajout du require

--- connexion.php
+++ connexion.php
@@ -1,19 +1,32 @@
 <?php
+ require('dbConnexion.php');
+ 
+if(isset($_POST['listName']) && isset($_POST['password'])&& isset($_POST['password2']))
+{
+	if($_POST['password'] == $_POST['password2'])
+	{
+		$listName = $_POST['listName'];
+		$password = md5($_POST['password']);
+		
+		$listName = htmlentities($listname);
+		
+		$sql = 'INSERT INTO LogList(listName, password) VALUES(:listName, :password))';
+	
+		$response = $dbh->prepare($sql);
+		$response->bindParam(':listName', $listName, PDO::PARAM_STR, 80);
+		$response->bindParam(':password', $password, PDO::PARAM_STR, 255);
+		$response->execute();
+	}
+	else
+	{
+		echo 'Les mots de passe ne correspondent pas.';
+		header('Location: connexion.html');
+	}
+}
+else
+{
+	echo 'Un des champs n\'est pas correct';
+	header('Location: connexion.html');
+}
 
-  $host='mysql.hostinger.fr';
-  $user='u632642010_root';
-  $pass='stkl_helb';
-  $db='helbbook';
-  	
-  $dsn = "mysql:host=$host;dbname=$db";
-  	
-  
-  try 
-  {
-  	$dbh = new PDO($dsn, $user, $pass); //db handle
-  } 
-  catch (PDOException $e) 
-  {
-  	die( "Erreur ! : " . $e->getMessage() );
-  }
 ?>
