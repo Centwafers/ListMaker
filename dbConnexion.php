63f383aa95dc975a612a633f723122b17257b53d
Centwafers <lenaertsm@hotmail.be>
2016-03-05 14:04:51 +0100

    création du fichier

--- dbConnexion.php
+++ dbConnexion.php
@@ -1,1 +1,29 @@
+<?php
 
+  $host='mysql.hostinger.fr';
+  $user='u632642010_root';
+  $pass='stkl_helb';
+  $db='u632642010_stkl';
+  	
+  $dsn = "mysql:host=$host;dbname=$db";
+  	
+  
+  try 
+  {
+    	$dbh = new PDO($dsn, $user, $pass); //db handle
+  } 
+  catch (PDOException $e) 
+  {
+    	die( "Erreur ! : " . $e->getMessage() );
+  }
+  
+	$sql = 'INSERT INTO LogList(listName, password) VALUES(:listName, :password)';
+	
+	$blabla = 'hahahahah';
+	$response = $dbh->prepare($sql);
+	$response->bindParam(':listName', $blabla, PDO::PARAM_STR, 80);
+	$response->bindParam(':password', $blabla, PDO::PARAM_STR, 255);
+	$response->execute();
+
+
+?>
