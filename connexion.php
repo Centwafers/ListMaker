2da3cc3567d4532c569a5bb7e3f6db02796d85ec
btalhaoui <btalhaoui@helb-prigogine.be>
2016-03-09 00:30:21 +0100

    Added files via upload

---
+++ connexion.php
@@ -0,0 +1,21 @@
+<?php
+ require('dbConnexion.php');
+$msg = "fail";
+if(isset($_POST['listName']) && isset($_POST['password']))
+{	
+		$listName = $_POST['listName'];
+		$password = md5($_POST['password']);
+		$listName = htmlentities($listname);
+		$response=$dbh->prepare('SELECT listName,password FROM LogList WHERE listName = :listName AND password = :password');
+		$response->bindParam(':listName', $listName, PDO::PARAM_STR, 80);
+		$response->bindParam(':password', $password, PDO::PARAM_STR, 255);
+        $response->execute();
+        $data=$response->fetch();
+			if ($data['password'] == $password){
+				$msg="success";
+			}
+}
+echo $msg;
+
+
+?>
