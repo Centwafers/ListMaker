2df9bfd1151d42388f173a9faeaa28ff71b6bce6
Noubexxx <robs_unique@hotmail.com>
2016-03-09 14:53:36 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,12 +1,15 @@
 <?php
-
-$stmt = $dbh->prepare("SELECT * FROM users WHERE (username=:username OR mail=:username) AND password=:password");
-				$stmt->bindParam('username',test,PDO::PARAM_STR,20);
-				$stmt->bindParam('password',test,PDO::PARAM_STR,20);
-				$stmt->execute();
-				
-				if($stmt->rowCount() == 1)
-				{
-				  echo 'success';
-				}
+	$stmt = $dbh->prepare("SELECT * FROM users WHERE username=:username AND password=:password");
+		$stmt->bindParam('username',"test",PDO::PARAM_STR,20);
+		$stmt->bindParam('password',"test",PDO::PARAM_STR,20);
+		$stmt->execute();
+		
+		if($stmt->rowCount() == 1)
+		{
+			echo 'success';
+		}
+		else
+		{
+			echo 'fail';
+		}
 ?>
