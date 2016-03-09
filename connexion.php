d1e5297825415cc7a5bd33c25eb752ad6d8b7c2f
Centwafers <lenaertsm@hotmail.be>
2016-03-09 14:11:46 +0100

    test

--- connexion.php
+++ connexion.php
@@ -4,7 +4,7 @@ $msg = "fail";
 if(isset($_GET['listName']) && isset($_GET['password']))
 {	
 		$listName = $_POST['listName'];
-		$password = md5($_POST['password']);
+		$password = $_POST['password'];
 		$listName = htmlentities($listname);
 		$response=$dbh->prepare('SELECT listName,password FROM LogList WHERE listName = :listName AND password = :password');
 		$response->bindParam(':listName', $listName, PDO::PARAM_STR, 80);
