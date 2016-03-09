c355c490c831014afda3f18bec5e8798fbe7487b
Centwafers <lenaertsm@hotmail.be>
2016-03-09 15:29:33 +0100

    Final correction normalement

--- connexionTest.php
+++ connexionTest.php
@@ -1,27 +1,23 @@
 <?php
   require('dbConnexion.php');
- $msg = "fail";
- if(isset($_GET['listName']) && isset($_GET['password']))
- {	
- 		$msg = 'isset ok';
- 		$listName = $_GET['listName'];
- 		$password = $_GET['password'];
- 		$listName = htmlentities($listName);
- 		$response=$dbh->prepare('SELECT listName, password FROM LogList WHERE listName = :listName AND password = :password');
- 		$response->bindValue(':listName', $listName, PDO::PARAM_STR);
- 		$response->bindValue(':password', $password, PDO::PARAM_STR);
-         	
-         	$response->execute();
-         
-         	$data=$response->fetch();
-         	
-         	var_dump($data);
-         	
- 		if ($data['password'] == $password){
- 			$msg="success";
- 		}
- }
- echo $msg;
- 
- 
- ?>
+  $msg = "fail";
+  if(isset($_POST['listName']) && isset($_POST['password']))
+  {	
+   	$listName = $_POST['listName'];
+   	$password = $_POST['password'];
+   	$listName = htmlentities($listName);
+   	$response=$dbh->prepare('SELECT listName, password FROM LogList WHERE listName = :listName AND password = :password');
+   	$response->bindValue(':listName', $listName, PDO::PARAM_STR);
+   	$response->bindValue(':password', $password, PDO::PARAM_STR);
+    
+    $response->execute();
+  
+    $data=$response->fetch();
+  
+   	if ($data['password'] == $password)
+   	{
+   			$msg="success";
+   	}
+  }
+  echo $msg;
+?>
