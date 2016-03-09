89bd7f2889c89a91d6ae1d9408eb52f1cfbb5f06
Centwafers <lenaertsm@hotmail.be>
2016-03-09 10:34:18 +0100

    exist nom de liste
    Sécurité si le nom de liste existe déjà

--- inscription.php
+++ inscription.php
@@ -1,5 +1,5 @@
 <?php
- require('dbConnexion.php');
+require('dbConnexion.php');
  
 if(isset($_POST['listName']) && isset($_POST['password'])&& isset($_POST['password2']))
 {
@@ -10,12 +10,24 @@ if(isset($_POST['listName']) && isset($_POST['password'])&& isset($_POST['passwo
 		
 		$listName = htmlentities($listname);
 		
-		$sql = 'INSERT INTO LogList(listName, password) VALUES(:listName, :password))';
-	
+		$sql = 'SELECT listName FROM LogList WHERE listName=:listName';
 		$response = $dbh->prepare($sql);
-		$response->bindParam(':listName', $listName, PDO::PARAM_STR, 80);
-		$response->bindParam(':password', $password, PDO::PARAM_STR, 255);
+		$response->bindParam(':listName', $listName, PDO::PARAM_STR,80);
 		$response->execute();
+		if(count($response) === 0)
+		{
+			$sql = 'INSERT INTO LogList(listName, password) VALUES(:listName, :password))';
+		
+			$response = $dbh->prepare($sql);
+			$response->bindParam(':listName', $listName, PDO::PARAM_STR, 80);
+			$response->bindParam(':password', $password, PDO::PARAM_STR, 255);
+			$response->execute();
+			echo 'Success';
+		}
+		else
+		{
+			echo 'Cette liste existe déjà';
+		}
 	}
 	else
 	{
