1c2feea43039344d08318401e7fe76506d59e772
Centwafers <lenaertsm@hotmail.be>
2016-03-09 15:20:47 +0100

    Update connexionTest.php

--- connexionTest.php
+++ connexionTest.php
@@ -8,15 +8,15 @@
  		$password = $_GET['password'];
  		$listName = htmlentities($listname);
  		$response=$dbh->prepare('SELECT listName, password FROM LogList WHERE listName = :listName AND password = :password');
- 		$response->bindParam(':listName', $listName, PDO::PARAM_STR, 80);
- 		$response->bindParam(':password', $password, PDO::PARAM_STR, 255);
+ 		$response->bindValue(':listName', $listName, PDO::PARAM_STR);
+ 		$response->bindValue(':password', $password, PDO::PARAM_STR);
          	
          	$response->execute();
-         	echo $response;
+         
          	$data=$response->fetch();
          	
          	var_dump($data);
-         	var_dump($response);
+         	
  		if ($data['password'] == $password){
  			$msg="success";
  		}
