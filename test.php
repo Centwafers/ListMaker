cae5b5a523f4433ad317455b099fb1f8ef0958e6
Centwafers <lenaertsm@hotmail.be>
2016-03-09 14:47:51 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+  require('bdConnection.php');
+  
+  //test insert into
+  $sql = 'INSERT INTO LogList(listName, password) VALUES(:listName, :password))';
+		
+	$response = $dbh->prepare($sql);
+	$response->bindParam(':listName', unTest, PDO::PARAM_STR, 80);
+	$response->bindParam(':password', unTest, PDO::PARAM_STR, 255);
+	$response->execute();
+	
+?>
