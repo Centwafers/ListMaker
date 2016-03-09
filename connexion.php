c5fd2a657a065eb8c52052ee9671fc039da78abd
Centwafers <lenaertsm@hotmail.be>
2016-03-09 14:21:57 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -10,11 +10,14 @@ if(isset($_GET['listName']) && isset($_GET['password']))
 		$response=$dbh->prepare('SELECT listName,password FROM LogList WHERE listName = :listName AND password = :password');
 		$response->bindParam(':listName', $listName, PDO::PARAM_STR, 80);
 		$response->bindParam(':password', $password, PDO::PARAM_STR, 255);
-        $response->execute();
-        $data=$response->fetch();
-			if ($data['password'] == $password){
-				$msg="success";
-			}
+        	
+        	$response->execute();
+        	$data=$response->fetch();
+        	
+        	var_dump($data);
+		if ($data['password'] == $password){
+			$msg="success";
+		}
 }
 echo $msg;
 
