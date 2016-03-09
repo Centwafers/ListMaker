cd8bb837a804c52916083feaed5cb9753784628a
Noubexxx <robs_unique@hotmail.com>
2016-03-09 14:56:22 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,4 +1,5 @@
 <?php
+	require('db_Connexion');
 	$stmt = $dbh->prepare("SELECT * FROM users WHERE username=:username AND password=:password");
 		$stmt->bindParam('username',"test",PDO::PARAM_STR,20);
 		$stmt->bindParam('password',"test",PDO::PARAM_STR,20);
