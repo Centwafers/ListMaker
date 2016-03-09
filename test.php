02193d9ddfe4d773028a7b9aa79e4cd84589cffe
Centwafers <lenaertsm@hotmail.be>
2016-03-09 14:56:45 +0100

    Update test.php

--- test.php
+++ test.php
@@ -5,8 +5,8 @@
   $sql = 'INSERT INTO LogList(listName, password) VALUES(:listName, :password)';
 		
 	$response = $dbh->prepare($sql);
-	$response->bindParam(':listName', unTest, PDO::PARAM_STR, 80);
-	$response->bindParam(':password', unTest, PDO::PARAM_STR, 255);
+	$response->bindParam(':listName', 'unTest', PDO::PARAM_STR, 80);
+	$response->bindParam(':password', 'unTest', PDO::PARAM_STR, 255);
 	$response->execute();
 	
 ?>
