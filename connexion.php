13f78b5eed09f2998d1139a0a10455d14d881fba
Noubexxx <robs_unique@hotmail.com>
2016-03-10 23:13:42 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,19 +1,18 @@
 <?php
   require('dbConnexion.php');
+  require('test_input.php');
   $msg = "fail";
   
   if(isset($_POST['listName']) && isset($_POST['password']))
   {	
-   	$listName = $_POST['listName'];
-   	$password = $_POST['password'];
-   	$listName = htmlentities($listName);
+   	$listName = test_input($_POST['listName']);
+   	$password = test_input($_POST['password']);
    	$response=$dbh->prepare('SELECT listName, password FROM LogList WHERE listName = :listName AND password = :password');
    	$response->bindValue(':listName', $listName, PDO::PARAM_STR);
    	$response->bindValue(':password', $password, PDO::PARAM_STR);
-    
-    	$response->execute();
+    $response->execute();
   
-    	$data=$response->fetch();
+    $data=$response->fetch();
   
    	if ($data['password'] == $password)
    	{
