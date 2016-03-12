d7783257035e6687600ab8e32f5c5b876058fe40
Noubexxx <robs_unique@hotmail.com>
2016-03-12 14:41:08 +0100

    Update creation.php

--- creation.php
+++ creation.php
@@ -1,44 +1,34 @@
 <?php
 require('dbConnexion.php');
-/*
+ 
 if(isset($_POST['listName']) && isset($_POST['password'])&& isset($_POST['password2']))
 {
 	if($_POST['password'] == $_POST['password2'])
 	{
-
-		//$listName = $_POST['listName'];
-		//$password = md5($_POST['password']);
+		$listName = $_POST['listName'];
+		$password = md5($_POST['password']);
 		
-
-
+		$listName = htmlentities($listName);
+		
 		$sql = 'SELECT listName FROM LogList WHERE listName=:listName';
 		$response = $dbh->prepare($sql);
 		$response->bindParam(':listName', $listName, PDO::PARAM_STR,80);
 		$response->execute();
 		
-		if($response->rowCount() === 0)
+		if(count($response) === 0)
 		{
-		*/
-		
-				$listName = "azodnzaodjaz";
-				$password = "adoaodjad";
-				
 			$sql = 'INSERT INTO LogList(listName, password) VALUES(:listName, :password))';
 		
 			$response = $dbh->prepare($sql);
 			$response->bindParam(':listName', $listName, PDO::PARAM_STR, 80);
 			$response->bindParam(':password', $password, PDO::PARAM_STR, 255);
-			$bol = $response->execute();
-			echo $bol;
-			echo "testttt";
-			echo 'success22';
-			/*
+			$response->execute();
+			echo 'success';
 		}
 		else
 		{
 			echo 'Cette liste existe déjà';
 		}
-
 	}
 	else
 	{
@@ -49,5 +39,5 @@ else
 {
 	echo 'Un des champs n\'est pas correct';
 }
-*/
+
 ?>
