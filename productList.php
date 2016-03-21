f98288f3fcb1c65d866fb107573851a83a8a657b
Centwafers <lenaertsm@hotmail.be>
2016-03-21 13:53:41 +0100

    Added files via upload

---
+++ productList.php
@@ -0,0 +1,15 @@
+<?php
+
+require('bdConnexion.php');
+
+if(isset($_GET['idLogList'])
+{
+	$sql = 'SELECT idProduct, quantity FROM ProductList WHERE idLogList = :idLogList';
+	$response = $dbh->prepare($sql);
+	$response->bindValue(':idLogList', $_GET['idLogList'], PDO::PARAM_INT);
+	$response->execute();
+	
+	var_dump($response);
+}
+
+?>
