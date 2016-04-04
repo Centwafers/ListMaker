4dd0948bd6a47dc627b70eab71a1c5b51ccdcaf4
MajdaSahloumi <majda.sahloumi@gmail.com>
2016-04-04 22:35:32 +0200

    design connexion final

--- js/connexion-bis.js
+++ js/connexion-bis.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
       },
       function(data, status){
           if(data == get_success){
+            successAnimation("#connexion-btn");
             document.location.href="home.html";
           }else{
             errorAnimation("#connexion-btn");
@@ -31,6 +32,10 @@ $(document).ready(function(){
 
   function errorAnimation(elemID){
       var elem = $(elemID);
+      var blue = "#5CBCF6";
+      var red = "#FF3333"
+
+      elem.animate({backgroundColor: red, borderColor: red}, 250);
 
       for (i = 0; i < 3; i++) {
           elem.animate({left: '-5%'}, 50);
@@ -38,6 +43,16 @@ $(document).ready(function(){
       }
 
       elem.animate({left: '0%'}, 25);
+      elem.animate({backgroundColor: blue, borderColor: blue}, 250);
+  }
+
+  function successAnimation(elemID){
+    var elem = $(elemID);
+    var green = "#47d147"
+
+    elem.animate({backgroundColor: green, borderColor: green}, 250);
+    elem.find("span").text("");
+    elem.find("span").addClass("fa fa-shopping-cart");
   }
 
 });
