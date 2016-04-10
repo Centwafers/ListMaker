671718e0c71f5aa74b093bfd191ab860698d0976
btalhaoui <btalhaoui@helb-prigogine.be>
2016-04-10 22:20:42 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -8,7 +8,7 @@
    	$listName = test_input($_GET['listName']);
    	$password = test_input($_GET['password']);
    	
-   	$response=$dbh->prepare('SELECT listName, password FROM LogList WHERE listName = :listName AND password = :password');
+   	$response=$dbh->prepare('SELECT * FROM LogList WHERE listName = :listName AND password = :password');
    	$response->bindValue(':listName', $listName, PDO::PARAM_STR);
    	$response->bindValue(':password', $password, PDO::PARAM_STR);
     $response->execute();
