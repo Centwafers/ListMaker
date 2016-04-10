0a959d7614097faa19e65372181e004031a3e004
btalhaoui <btalhaoui@helb-prigogine.be>
2016-04-10 21:11:11 +0200

    Update creation.php
    adding hash

--- creation.php
+++ creation.php
@@ -18,11 +18,13 @@ if(isset($_POST['listName']) && isset($_POST['password'])&& isset($_POST['passwo
 		
 		if($response->rowCount() === 0)
 		{
-			$sql = 'INSERT INTO LogList(listName, password) VALUES(:listName, :password)';
+			$sql = 'INSERT INTO LogList(listName, password, hashSession) VALUES(:listName, :password, :hash)';
 		
 			$response = $dbh->prepare($sql);
 			$response->bindParam(':listName', $listName, PDO::PARAM_STR, 80);
 			$response->bindParam(':password', $password, PDO::PARAM_STR, 255);
+			$hash = md5('STKL20/20'.$listname);
+			$response->bindParam(':hash', $hash, PDO::PARAM_STR, 255);
 			$response->execute();
 			echo 'success';
 		}
