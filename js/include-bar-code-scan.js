d3d0a1a1ec494eb93d6ae800a870fdcd59685da9
btalhaoui <btalhaoui@helb-prigogine.be>
2016-04-10 22:54:25 +0200

    Update include-bar-code-scan.js

--- js/include-bar-code-scan.js
+++ js/include-bar-code-scan.js
@@ -1,3 +1,22 @@
 $(function(){
-	$("#bar-code-scan").load("bar-code-scan.html"); 
+	$("#bar-code-scan").load("bar-code-scan.html");
+	 $("#scan-btn").click(function(){
+		   alert("izi");
+			cordova.plugins.barcodeScanner.scan(
+		  function (result) {
+		    if(!result.cancelled)
+		    {
+		      alert("Barcode type is: " + result.format);
+		      alert("Decoded text is: " + result.text);
+		    }
+		    else
+		    {
+		      alert("You have cancelled scan");
+		    }
+		  },
+		  function (error) {
+		      alert("Scanning failed: " + error);
+		  }
+		);   
+	});
 });
