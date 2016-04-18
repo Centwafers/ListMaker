f156288612a0753671ceb51b2723bc8cf9a158ce
Centwafers <lenaertsm@hotmail.be>
2016-04-18 22:39:59 +0200

    Update addProduct.php

--- addProduct.php
+++ addProduct.php
@@ -1,24 +1,27 @@
 <?php
-//Pas fini!!!
+
 require('dbConnexion.php');
 
-if(isset($_GET['idLogList']) && isset($_GET['idProduct'])) {
+if(isset($_GET['idLogList']) && isset($_GET['idProduct']) && isset($_GET['quantity']) && isset($_GET['addedBy'])) {
 
   $sql = 'SELECT idProduct FROM MarketList WHERE idProduct = :idProduct';
   $response = $dbh->prepare($sql);
   $response->bindValue(':idProduct', $_GET['idProduct'], PDO::PARAM_INT);
   $response->execute();
   
-  
-	$sql = 'SELECT idProduct, quantity FROM ConsumerList WHERE idLogList = :idLogList';
-	$response = $dbh->prepare($sql);
-	$response->bindValue(':idLogList', $_GET['idLogList'], PDO::PARAM_INT);
-	$response->execute();
-	
-	foreach($response as $elem)
-	{
-		echo json_encode($elem);
-	}
+  if($response->rowCount() === 1){
+    $sql = "INSERT INTO ConsumerList VALUES(':idLogList', ':idProduct', '$_GET['quantity']', '$_GET['addedBy']')"
+    $response = $dbh->prepare($sql);
+    $response->bindValue(':idLogList', $_GET['idLogList'], PDO::PARAM_INT);
+    $response->bindValue(':idProduct', $_GET['idProduct'], PDO::PARAM_INT);
+    $response->bindValue(':quantity', $_GET['quantity'], PDO::PARAM_INT);
+    $response->bindValue(':addedBy', $_GET['addedBy'], PDO::PARAM_INT);
+    $response->execute();
+    
+    return true;
+  }
+  else{
+    return false;
+  }
 }
-
 ?>
