d0ef344dbd7f711667e7fd2ae822cd1a9186e7d8
Centwafers <lenaertsm@hotmail.be>
2016-04-26 20:09:37 +0200

    Update addProductTest.php

--- addProductTest.php
+++ addProductTest.php
@@ -17,14 +17,14 @@ if(isset($_GET['hashSession']) && isset($_GET['idProduct']) && isset($_GET['quan
   if($response1->rowCount()===1 && $response->rowCount()===1)
   {
     $response1 = $response1->fetch();
-    //echo $response1['id'].' '.$_GET['idProduct'].' '.$_GET['quantity'].' '.$_GET['addedBy'];
-    $sql = "INSERT INTO 'ConsumerList' VALUES(':idLogList', ':idProduct', ':quantity', ':addedBy')";
+    echo $response1['id'].' '.$_GET['idProduct'].' '.$_GET['quantity'].' '.$_GET['addedBy'].'</br>';
+    //$sql = "INSERT INTO 'ConsumerList' VALUES(':idLogList', ':idProduct', ':quantity', ':addedBy')";
     $sql = "INSERT INTO `ConsumerList`(`idLoglist`, `idProduct`, `quantity`, `addedBy`) VALUES (':idLogList',':idProduct',':quantity',':addedBy')";
     $response = $dbh->prepare($sql);
-    $response->bindParam(':idLogList', $response1['id']);
-    $response->bindParam(':idProduct', $_GET['idProduct']);
-    $response->bindParam(':quantity', $_GET['quantity']);
-    $response->bindParam(':addedBy', $_GET['addedBy']);
+    $response->bindValue(':idLogList', $response1['id']);
+    $response->bindValue(':idProduct', $_GET['idProduct']);
+    $response->bindValue(':quantity', $_GET['quantity']);
+    $response->bindValue(':addedBy', $_GET['addedBy']);
     echo $response->execute();
   }
   else
