03624be974bc1d88b77e76d8ae1fec7450e21348
Centwafers <lenaertsm@hotmail.be>
2016-04-26 19:50:42 +0200

    Update addProductTest.php

--- addProductTest.php
+++ addProductTest.php
@@ -18,7 +18,7 @@ if(isset($_GET['hashSession']) && isset($_GET['idProduct']) && isset($_GET['quan
   {
     $response1 = $response1->fetch();
     echo $response1['id'].' '.$_GET['idProduct'].' '.$_GET['quantity'].' '.$_GET['addedBy'];
-    $sql = "INSERT INTO ConsumerList VALUES(':idLogList', ':idProduct', ':quantity', ':addedBy')";
+    $sql = "INSERT INTO ConsumerList ('idLogList', 'idProduct', 'quantity', 'addedBy') VALUES(':idLogList', ':idProduct', ':quantity', ':addedBy')";
     $response = $dbh->prepare($sql);
     $response->bindParam(':idLogList', $response1['id']);
     $response->bindParam(':idProduct', $_GET['idProduct']);
