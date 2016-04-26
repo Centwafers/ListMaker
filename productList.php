c045502116023712e95bb17fdd655402284de1d1
Centwafers <lenaertsm@hotmail.be>
2016-04-26 14:28:20 +0200

    Update productList.php

--- productList.php
+++ productList.php
@@ -5,9 +5,9 @@ require('dbConnexion.php');
 if(isset($_GET['idLogList']))
 //if(isset($_GET['hashDeSession']))
 {
-	$sql = 'SELECT idProduct, quantity FROM ProductList WHERE idLogList = :idLogList';
+	$sql = 'SELECT idProduct, quantity FROM ProductList WHERE idLoglist = :idLoglist';
 	$response = $dbh->prepare($sql);
-	$response->bindValue(':idLogList', $_GET['idLogList'], PDO::PARAM_INT);
+	$response->bindValue(':idLoglist', $_GET['idLogList'], PDO::PARAM_INT);
 	$response->execute();
 	
 	foreach($response as $elem)
