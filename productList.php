bf7d03c1c3c3576ab16273db224cd8d66d8e3d6b
Centwafers <lenaertsm@hotmail.be>
2016-04-26 14:46:46 +0200

    Update productList.php

--- productList.php
+++ productList.php
@@ -2,11 +2,16 @@
 
 require('dbConnexion.php');
 
-if(isset($_GET['idLoglist']))
-//if(isset($_GET['hashDeSession']))
+//if(isset($_GET['idLoglist']))
+if(isset($_GET['hashSession']))
 {
+	$sql = 'SELECT hashSession FROM LogList WHERE hashSession = :hashSession'
+	$response = $dbh->prepare($sql);
+	$response->bindValue(':hashSession', $_GET['hashSession'], PDO::PARAM_STR)
+	$response->execute();
+	echo rowCount($response);
 	
-	$sql = 'SELECT idProduct, quantity FROM ConsumerList WHERE idLoglist = :idLoglist';
+	$sql = 'SELECT idProduct, quantity FROM ConsumerList WHERE hashSession = :hashSession';
 	$response = $dbh->prepare($sql);
 	$response->bindValue(':idLoglist', $_GET['idLoglist'], PDO::PARAM_INT);
 	$response->execute();
