2e25c236994fe8bf8d420ea66a038fccee97ed79
Centwafers <lenaertsm@hotmail.be>
2016-04-26 15:05:01 +0200

    Update productList.php

--- productList.php
+++ productList.php
@@ -15,7 +15,7 @@ if(isset($_GET['hashSession']))
 	var_dump($response);
 	$sql = "SELECT idProduct, quantity FROM ConsumerList WHERE idLogList = :idLoglist";
 	$response2 = $dbh->prepare($sql);
-	$response2->bindValue(':idLoglist', $response['idLogList'], PDO::PARAM_INT);
+	$response2->bindValue(':idLoglist', $response['id'], PDO::PARAM_INT);
 	$response2->execute();
 	
 	foreach($response as $elem)
