49b112f79414a3ca7034e853d06e0cbf683f58c7
btalhaoui <btalhaoui@helb-prigogine.be>
2016-04-26 16:12:04 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -13,11 +13,12 @@ $(document).ready(function(){
 		type: "GET",
 		success:function(data){
 			console.log(data);
+			var obj = JSON.parse(data);
 			var productL;
 			 for (var i = 0; i < data.length; i++) {
 					productL = $('<li><a href="#product-detail" data-transition="pop"><img src="./img/productPicture/bread.png">');
-					productL.append("<h2>"+data[i].nameProduct+"</h2>");
-					productL.append("<p>"+data[i].addedBy+"</p>");
+					productL.append("<h2>"+obj[i].nameProduct+"</h2>");
+					productL.append("<p>"+obj[i].addedBy+"</p>");
 					productL.append('<span class="ui-li-count">2</span></a><a href="#" data-icon="star">Ajouter aux favoris</a></li>');
 					$('#listContent').append(productL);
 		    }
