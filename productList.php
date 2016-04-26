2cbdd54b0bd4bc5dd2ef619838fb3a4587299c4f
btalhaoui <btalhaoui@helb-prigogine.be>
2016-04-26 18:02:02 +0200

    Update productList.php

--- productList.php
+++ productList.php
@@ -31,11 +31,11 @@ if(isset($_GET['hashSession']))
 			
 			//$userListDetails=$userListDetails->fetchAll();
 			//echo json_encode($userListDetails);
-		//	$json = '{';
+			$json = '{';
 			//$result = array();
 			foreach($userListDetails as $oneDetails)
 			{
-				//$json .= '"'.$oneDetails['idProduct'].'"'.':';
+				$json .= '"'.$oneDetails['idProduct'].'"'.':';
 				$array = array(
 					'idProduct'	=>$oneDetails['idProduct'],
 					'nameProduct'	=>$oneDetails['nameProduct'],
@@ -46,13 +46,13 @@ if(isset($_GET['hashSession']))
 				);
 				 //array_push($result,$array);
 				echo json_encode($array, JSON_PRETTY_PRINT);
-				//$json .= ', ';
+				$json .= ', ';
 			}
 		//	echo json_encode($result);
 
-			/*$json = substr($json, 0, -1);
+			$json = substr($json, 0, -1);
 			$json .= '}';
-			echo $json;*/
+			echo $json;
 		}
 	}
 }
