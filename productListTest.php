093dd566ba490342b4fad030dd15ee42efd652b5
Centwafers <lenaertsm@hotmail.be>
2016-04-26 18:38:43 +0200

    Update productListTest.php

--- productListTest.php
+++ productListTest.php
@@ -16,7 +16,7 @@ if(isset($_GET['hashSession']))
 		$userList = $dbh->prepare($sql);
 		$userList->bindValue(':idLoglist', $user['id'], PDO::PARAM_INT);
 		$userList->execute();
-		
+		$json = '{';
 		foreach($userList as $oneProduct)
 		{
 			
@@ -29,27 +29,13 @@ if(isset($_GET['hashSession']))
 			$userListDetails->bindValue(':idLogList', $user['id'], PDO::PARAM_INT);
 			$userListDetails->execute();
 			
-			//$userListDetails=$userListDetails->fetchAll();
-			//header("content-type:application/json");
-			//echo json_encode($userListDetails->fetchAll(PDO::FETCH_NUM));
-
-			//echo json_encode($userListDetails);
 			
-			$json = '{';
 			$result = array();
 			foreach($userListDetails as $oneDetails)
 			{
-				
-				$json .= '"'.$oneDetails['idProduct'].'"'.':{'.
-					'"idProduct":"'.$oneDetails['idProduct'].'",'.
-					'"nameProduct":"'.$oneDetails['nameProduct'].'",'.
-					'"type":"'.$oneDetails['type'].'",'.
-					'"price":"'.$oneDetails['price'].'",'.
-					'"unity":"'.$oneDetails['unity'].'",'.
-					'"quantity":"'.$oneDetails['quantity'].'"'.
-					'},';
+
 					
-			/*	$json .= '"'.$oneDetails['idProduct'].'"'.':';
+				$json .= '"'.$oneDetails['idProduct'].'"'.':';
 				$array = array(
 					'idProduct'	=>$oneDetails['idProduct'],
 					'nameProduct'	=>$oneDetails['nameProduct'],
@@ -58,16 +44,17 @@ if(isset($_GET['hashSession']))
 					'unity'		=>$oneDetails['unity'],
 					'quantity'	=>$oneDetails['quantity']
 				);
-				 array_push($result,$array);*/
-			//	$json .= ', ';
+				 array_push($result,$array);
+				$json .= ', ';
 				
 			}
 			
 	
 			$json .= substr($json, 0, -1);
-			echo '}';
-//			echo $json;
+
+
 		}
+		echo '}';
 	}
 }
 ?>
