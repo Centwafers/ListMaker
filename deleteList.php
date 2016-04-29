3511a92205eec088225433a7b8e49b33f833f118
Centwafers <lenaertsm@hotmail.be>
2016-04-29 18:53:36 +0200

    Update deleteList.php

--- deleteList.php
+++ deleteList.php
@@ -1,5 +1,15 @@
 <?php
 
+  require('dbConnexion.php');
+  
   echo time();
+  
+  echo strtotime('1998-07-12 22:30:00').'</br>';
+  echo abs(time()-strtotime('1998-07-12 22:30:00')).'</br>';
+  $sql = 'SELECT * FROM LogList';
+  $response=$dbh->execute($sql);
+  $reponse = $response->fetch();
+  echo $reponse['lastUse'].'</br>';
+  echo strtotime($reponse['lastUse']).'</br>';
 
 ?>
