adb8176053dcf4a394f4a833db59af55094c08aa
Centwafers <lenaertsm@hotmail.be>
2016-04-29 22:05:35 +0200

    Update deleteList.php

--- deleteList.php
+++ deleteList.php
@@ -10,12 +10,12 @@
     if($result>14)
     {
       $sqlDel = 'DELETE FROM LogList WHERE id = :id';
-      $response = $dbh->prepare();
+      $response = $dbh->prepare($sqlDel);
       $response->bindValue(':id', $row['id'], PDO::PARAM_INT);
       $response->execute();
       
       $sqlDel = 'DELETE FROM ConsumerList WHERE id = :id';
-      $response = $dbh->prepare();
+      $response = $dbh->prepare($sqlDel);
       $response->bindValue(':id', $row['id'], PDO::PARAM_INT);
       $response->execute();
     }
