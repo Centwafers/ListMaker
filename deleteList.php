6ece131752abc52ff7b542a49b5630edbb9c9b0f
Centwafers <lenaertsm@hotmail.be>
2016-04-29 19:08:02 +0200

    Update deleteList.php

--- deleteList.php
+++ deleteList.php
@@ -7,10 +7,10 @@
   echo strtotime('1998-07-12 22:30:00').'</br>';
   echo abs(time()-strtotime('1998-07-12 22:30:00')).'</br>';
   $sql = 'SELECT * FROM LogList';
-  $dbh->prepare($sql);
-  $response=$dbh->execute();
-  $reponse = $response->fetch();
-  echo $reponse['lastUse'].'</br>';
-  echo strtotime($reponse['lastUse']).'</br>';
+  $response = $dbh->prepare($sql);
+  $response->execute();
+  $response = $response->fetch();
+  echo $repsonse['lastUse'].'</br>';
+  echo strtotime($response['lastUse']).'</br>';
 
 ?>
