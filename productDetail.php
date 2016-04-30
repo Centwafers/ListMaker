e6b14f2fa7994e34d8b26f87cfd823ee67b3ba58
Centwafers <lenaertsm@hotmail.be>
2016-04-30 10:22:22 +0200

    Update and rename productDetails.php to productDetail.php

---
+++ productDetail.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_get['idProduct']))
+{
+  $sql = 'SELECT * FROM MarketList WHERE idProduct = :idProduct';
+  $product = $dbh->prepare($sql);
+  $product->bindValue(':idProduct', $_get['idProduct'], PDO::PARAM_INT);
+  $product->execute();
+  if($product->rowCount()===1)
+  {
+    $product=$product->fetch();
+    echo json_encode($product);
+  }
+}
+?>
