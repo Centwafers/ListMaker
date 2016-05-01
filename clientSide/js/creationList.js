a6cdfbb9ee55cb863cdda73f3c670017e513613c
Majda Sahloumi <majda.sahloumi@gmail.com>
2016-05-01 19:54:22 +0200

    Update creationList.js

--- clientSide/js/creationList.js
+++ clientSide/js/creationList.js
@@ -7,22 +7,22 @@ document.getElementById("creationBtn").onclick = function blockForm() {
     var serverAdress = 'http://listmaker-stkl.esy.es/';
     var file = 'creationList.php';
 
-    if (listName.length === 0) {
-        creationErrorAnimation("Champ vide");
-        // return alert("Veuillez entrer le nom de votre liste");
-    } else if (listName.length < 3) {
-        creationErrorAnimation("Nom trop court");
-        // return alert("Le nom de la liste doit comporter au moins 3 caractères");
-    } else if (password.length === 0) {
-        creationErrorAnimation("Champ vide");
-        // return alert("Veuillez entrer votre mot de passe");
-    } else if (password.length < 3) {
-        creationErrorAnimation("Mot de passe trop court");
-        // return alert("Votre mot de passe est beaucoup trop court"); // à modifier si MD5 côté client
-    } else if (!(password == password2)) {
-        creationErrorAnimation("Mots de passe différents");
-        // return alert("Les deux mots de passes ne correspondent pas");
-    } else {
+    if (listName.length===0){
+        creationErrorAnimation("Tous les champs doivent être remplis");
+    }
+    else if (listName.length<3){
+        creationErrorAnimation("Le nom de la liste doit comporter au moins 8 caractères");
+    }
+    else if (password.length===0){
+        creationErrorAnimation("Tous les champs doivent être remplis");
+    }
+    else if (password.length<3){
+        creationErrorAnimation("Le mot de passe doit comporter au moins 8 caractères");
+    }
+    else if (!(password==password2)){
+        creationErrorAnimation("Les mots de passe sont différents");
+    }
+    else{
 
         var get_success = 'success';
 
@@ -36,7 +36,8 @@ document.getElementById("creationBtn").onclick = function blockForm() {
                     creationSuccessAnimation();
                     // alert("Votre liste a bien été enregistrée");
                     document.location.href = "connection.html";
-                } else {
+                } 
+                else {
                     creationErrorAnimation("Cette liste existe déjà");
                     //alert(resultat);
                 }
