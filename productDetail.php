df9a2112f3844e18ea83aec6ed2239ebe7d0b730
Centwafers <lenaertsm@hotmail.be>
2016-05-05 20:29:29 +0200

    Update productDetail.php

--- productDetail.php
+++ productDetail.php
@@ -4,16 +4,14 @@ require('dbConnexion.php');
 
 header("Access-Control-Allow-Origin: *");
 
-if (isset($_GET['idProduct']))
-{
-  $sql = 'SELECT * FROM MarketList WHERE idProduct = :idProduct';
-  $product = $dbh->prepare($sql);
-  $product->bindValue(':idProduct', $_GET['idProduct'], PDO::PARAM_INT);
-  $product->execute();
-  if ($product->rowCount()===1)
-  {
-    $product=$product->fetch();
-    echo json_encode($product);
-  }
+if (isset($_GET['idProduct'])) {
+    $sql     = 'SELECT * FROM MarketList WHERE idProduct = :idProduct';
+    $product = $dbh->prepare($sql);
+    $product->bindValue(':idProduct', $_GET['idProduct'], PDO::PARAM_INT);
+    $product->execute();
+    if ($product->rowCount() === 1) {
+        $product = $product->fetch();
+        echo json_encode($product);
+    }
 }
 ?>
