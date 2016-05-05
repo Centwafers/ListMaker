7632ab56fac9e90b57cdbc2b29e7e7bec32ca38e
Centwafers <lenaertsm@hotmail.be>
2016-05-05 20:28:43 +0200

    Update deleteProduct.php

--- deleteProduct.php
+++ deleteProduct.php
@@ -1,21 +1,20 @@
 <?php
 require('dbConnexion.php');
 header("Access-Control-Allow-Origin: *");
-if(isset($_GET['hashSession']) && isset($_GET['idProduct'])) {
-  $sql = 'SELECT id, hashSession FROM LogList WHERE hashSession = :hashSession';
-  $response1 = $dbh->prepare($sql);
-  $response1->bindValue(':hashSession', $_GET['hashSession']);
-  $response1->execute();
-  
-  if($response1->rowCount()===1)
-  {
-    $response1 = $response1->fetch();
-    $sql = 'DELETE FROM ConsumerList WHERE idProduct = :idProduct AND idLogList = :idLogList';
-    $response = $dbh->prepare($sql);
-    $response->bindValue(':idProduct', $_GET['idProduct'], PDO::PARAM_INT);
-    $response->bindValue(':idLogList', $response1['id'], PDO::PARAM_INT);
-    $response->execute();
-  }
+if (isset($_GET['hashSession']) && isset($_GET['idProduct'])) {
+    $sql       = 'SELECT id, hashSession FROM LogList WHERE hashSession = :hashSession';
+    $response1 = $dbh->prepare($sql);
+    $response1->bindValue(':hashSession', $_GET['hashSession']);
+    $response1->execute();
+    
+    if ($response1->rowCount() === 1) {
+        $response1 = $response1->fetch();
+        $sql       = 'DELETE FROM ConsumerList WHERE idProduct = :idProduct AND idLogList = :idLogList';
+        $response  = $dbh->prepare($sql);
+        $response->bindValue(':idProduct', $_GET['idProduct'], PDO::PARAM_INT);
+        $response->bindValue(':idLogList', $response1['id'], PDO::PARAM_INT);
+        $response->execute();
+    }
 }
-  
+
 ?>
