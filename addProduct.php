7787929222dfbc99845ad9a0f52afdd61caa3d97
Centwafers <lenaertsm@hotmail.be>
2016-05-05 17:38:12 +0200

    Update addProduct.php

--- addProduct.php
+++ addProduct.php
@@ -1,31 +1,28 @@
 <?php
-
 require('dbConnexion.php');
 header("Access-Control-Allow-Origin: *");
-if(isset($_GET['hashSession']) && isset($_GET['idProduct']) && isset($_GET['quantity']) && isset($_GET['addedBy'])) {
-
-  $sql = 'SELECT id, hashSession FROM LogList WHERE hashSession = :hashSession';
-  $user = $dbh->prepare($sql);
-  $user->bindValue(':hashSession', $_GET['hashSession']);
-  $user->execute();
-  
-  $sql = 'SELECT idProduct FROM MarketList WHERE idProduct = :idProduct';
-  $product = $dbh->prepare($sql);
-  $product->bindValue(':idProduct', $_GET['idProduct'], PDO::PARAM_INT);
-  $product->execute();
-  
-  if($user->rowCount()===1 && $product->rowCount()===1)
-  {
-    $user = $user->fetch();
-    $sql = "DELETE FROM ConsumerList WHERE idLogList=:idLogList and idProduct=:idProduct;INSERT INTO `ConsumerList`(`idLoglist`, `idProduct`, `quantity`, `addedBy`) 
-            VALUES (:idLogList,:idProduct,:quantity,:addedBy)";
+if (isset($_GET['hashSession']) && isset($_GET['idProduct']) && isset($_GET['quantity']) && isset($_GET['addedBy'])) {
+    $sql  = 'SELECT id, hashSession FROM LogList WHERE hashSession = :hashSession';
+    $user = $dbh->prepare($sql);
+    $user->bindValue(':hashSession', $_GET['hashSession']);
+    $user->execute();
+    
+    $sql     = 'SELECT idProduct FROM MarketList WHERE idProduct = :idProduct';
     $product = $dbh->prepare($sql);
-    $product->bindValue(':idLogList', $user['id']);
-    $product->bindValue(':idProduct', $_GET['idProduct']);
-    $product->bindValue(':quantity', $_GET['quantity']);
-    $product->bindValue(':addedBy', $_GET['addedBy']);
-    echo $user['id'].' '.$_GET['idProduct'].' '.$_GET['quantity'].' '.$_GET['addedBy'].'</br>';
+    $product->bindValue(':idProduct', $_GET['idProduct'], PDO::PARAM_INT);
     $product->execute();
-  }
+    
+    if ($user->rowCount() === 1 && $product->rowCount() === 1) {
+        $user    = $user->fetch();
+        $sql     = "DELETE FROM ConsumerList WHERE idLogList=:idLogList and idProduct=:idProduct;INSERT INTO `ConsumerList`(`idLoglist`, `idProduct`, `quantity`, `addedBy`) 
+            VALUES (:idLogList,:idProduct,:quantity,:addedBy)";
+        $product = $dbh->prepare($sql);
+        $product->bindValue(':idLogList', $user['id']);
+        $product->bindValue(':idProduct', $_GET['idProduct']);
+        $product->bindValue(':quantity', $_GET['quantity']);
+        $product->bindValue(':addedBy', $_GET['addedBy']);
+        echo $user['id'] . ' ' . $_GET['idProduct'] . ' ' . $_GET['quantity'] . ' ' . $_GET['addedBy'] . '</br>';
+        $product->execute();
+    }
 }
 ?>
