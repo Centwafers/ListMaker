71b5a2223375bc9d80c20f68ff14b84c03825dd4
Centwafers <lenaertsm@hotmail.be>
2016-05-05 17:39:55 +0200

    Update autocomplete.php

--- autocomplete.php
+++ autocomplete.php
@@ -2,27 +2,27 @@
 require('dbConnexion.php');
 header("Access-Control-Allow-Origin: *");
 if (!isset($_GET['keyword'])) {
-	die();
-}else{
-$keyword = $_GET['keyword'];
-	$stmt = $dbh->prepare("SELECT idProduct,nameProduct FROM `MarketList` WHERE nameProduct LIKE ?");
-
+    die();
+} else {
+    $keyword = $_GET['keyword'];
+    $stmt    = $dbh->prepare("SELECT idProduct,nameProduct FROM `MarketList` WHERE nameProduct LIKE ?");
+    
     $keyword = $keyword . '%';
     $stmt->bindParam(1, $keyword, PDO::PARAM_STR, 100);
     $isQueryOk = $stmt->execute();
-  
+    
     $results = array();
     
     if ($isQueryOk) {
-      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     } else {
-      trigger_error('Error executing statement.', E_USER_ERROR);
+        trigger_error('Error executing statement.', E_USER_ERROR);
     }
-
-
-    $data =$results;
-
-
-echo json_encode($data);
+    
+    
+    $data = $results;
+    
+    
+    echo json_encode($data);
 }
 ?>
