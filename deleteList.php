dbb3c5b1321ded1fef8b5003f145828023cfa6bd
Centwafers <lenaertsm@hotmail.be>
2016-05-05 20:27:23 +0200

    Update deleteList.php

--- deleteList.php
+++ deleteList.php
@@ -1,26 +1,22 @@
 <?php
+require('dbConnexion.php');
 
-  require('dbConnexion.php');
-  
-  $now = time();
-  
-  $sql = 'SELECT * FROM LogList';
-  foreach($dbh->query($sql) as $row){
-    $result = (time()-strtotime($row['lastUse']))/60/60/24;
-    if($result>14)
-    {
-      $sqlDel = 'DELETE FROM LogList WHERE id = :id';
-      $response = $dbh->prepare($sqlDel);
-      $response->bindValue(':id', $row['id'], PDO::PARAM_INT);
-      $response->execute();
-      
-      $sqlDel = 'DELETE FROM ConsumerList WHERE id = :id';
-      $response = $dbh->prepare($sqlDel);
-      $response->bindValue(':id', $row['id'], PDO::PARAM_INT);
-      $response->execute();
+$now = time();
+
+$sql = 'SELECT * FROM LogList';
+foreach ($dbh->query($sql) as $row) {
+    $result = (time() - strtotime($row['lastUse'])) / 60 / 60 / 24;
+    if ($result > 14) {
+        $sqlDel   = 'DELETE FROM LogList WHERE id = :id';
+        $response = $dbh->prepare($sqlDel);
+        $response->bindValue(':id', $row['id'], PDO::PARAM_INT);
+        $response->execute();
+        
+        $sqlDel   = 'DELETE FROM ConsumerList WHERE id = :id';
+        $response = $dbh->prepare($sqlDel);
+        $response->bindValue(':id', $row['id'], PDO::PARAM_INT);
+        $response->execute();
     }
-  }
-
-  
+}
 
 ?>
