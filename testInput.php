bb511b25cbbd62dc639c572e31586e4e85dc8f67
Centwafers <lenaertsm@hotmail.be>
2016-05-05 20:32:15 +0200

    Update testInput.php

--- testInput.php
+++ testInput.php
@@ -1,18 +1,14 @@
 <?php
-
-	function testInput($data)
-	{
-		$data = trim($data);
-		$data = htmlspecialchars($data);
-		$data = stripslashes($data);
-		return $data;
-	}
-
-	function testMd5($data)
-	{
-		define('R_MD5_MATCH', '/^[a-f0-9]{32}$/i');
-
-		return preg_match(R_MD5_MATCH, $data);
-	}
-
+function testInput($data)
+{
+    $data = trim($data);
+    $data = htmlspecialchars($data);
+    $data = stripslashes($data);
+    return $data;
+}
+function testMd5($data)
+{
+    define('R_MD5_MATCH', '/^[a-f0-9]{32}$/i');
+    return preg_match(R_MD5_MATCH, $data);
+}
 ?>
