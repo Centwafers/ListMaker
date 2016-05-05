f4ba6ab58c42684ad7f14dbcfcb22c604f74ad32
Centwafers <lenaertsm@hotmail.be>
2016-05-05 20:26:29 +0200

    Update dbConnexion.php

--- dbConnexion.php
+++ dbConnexion.php
@@ -1,19 +1,16 @@
 <?php
-  $host='mysql.hostinger.fr';
-  $user='u632642010_root';
+$host = 'localhost';
+$user = 'u632642010_root';
 
-  $pass='stkl_helb';
-  $db='u632642010_stkl';
-  	
-  $dsn = "mysql:host=$host;dbname=$db";
-  	
-  
-  try 
-  {
-    	$dbh = new PDO($dsn, $user, $pass); //db handle
-  } 
-  catch (PDOException $e) 
-  {
-    	die( "Erreur ! : " . $e->getMessage() );
-  }
+$pass = '';
+$db   = 'u632642010_stkl';
+
+$dsn = "mysql:host=$host;dbname=$db";
+
+try {
+    $dbh = new PDO($dsn, $user, $pass); //db handle
+}
+catch (PDOException $e) {
+    die("Erreur ! : " . $e->getMessage());
+}
 ?>
